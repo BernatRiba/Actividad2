429dfc4c85e4e11275784e26ee3264e31f4dbdea
smookees <33909400+smookees@users.noreply.github.com>
2022-12-16 14:21:16 +0100

    Act2 Tests

--- act2/src/GameTest.java
+++ act2/src/GameTest.java
@@ -6,9 +6,8 @@ class GameTest {
     void factorizerTest() {
         int arrayTest[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
         int arrayTestResult[] = {1,2,3,4,5,5,7,6,6,7,11,8,13};
-        Game juego = new Game("","");
         for (int i=0; i<arrayTest.length; i++) {
-            assertEquals(arrayTestResult[i],juego.factorizer(arrayTest[i]));
+            assertEquals(arrayTestResult[i],Game.factorizer(arrayTest[i]));
         }
     }
 
@@ -16,9 +15,8 @@ class GameTest {
     void romanicToNumTest() {
         String arrayTest[] = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII"};
         int arrayTestResult[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-        Game juego = new Game("","");
         for (int i=0; i<arrayTest.length; i++) {
-            assertEquals(arrayTestResult[i],juego.romanicToInteger(arrayTest[i]));
+            assertEquals(arrayTestResult[i], Game.romanicToInteger(arrayTest[i]));
         }
     }
 
@@ -27,5 +25,7 @@ class GameTest {
         String romanicPointsP1 = "VI";
         String romanicPointsP2 = "XIII";
         Game juego = new Game(romanicPointsP1,romanicPointsP2);
+        assertEquals(juego.coinsEarnedP1,5);
+        assertEquals(juego.coinsEarnedP2,13);
     }
 }
